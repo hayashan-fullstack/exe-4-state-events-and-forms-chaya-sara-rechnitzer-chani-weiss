185dfa9561907f314df54c4f12a220ae8924d2ae
chayasara <57223241+chayasara@users.noreply.github.com>
2019-12-16 18:20:28 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Chaya Sara ', num: '0548590674' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNum, setNewNum ] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const nameObject = {
+      name: newName,
+      num: newNum
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+    setNewNum('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+  const handleNumChange = (event) => {
+    console.log(event.target.value)
+    setNewNum(event.target.value)
+  }
+
+  const Rows = (props) => {
+  return(
+  <div>
+    <li>{props.name} {props.num}</li>
+  </div>)
+}
+  
+  const Print=(props)=>{
+    return (props.persons.map(person=>  <Rows key={person.name} name={person.name} num={person.num}/>))
+  }
+  
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          Name:  <br/><input Name={newName} onChange={handleNameChange}/>
+          <br/><br/>
+          Phone Number:  <br/><input num={newNum} onChange={handleNumChange}/>
+        </div>
+        <div>
+        <br/>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {<Print persons={persons}/>}
+      </div>
+    </div>
+  )
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById("root") );
+
+
